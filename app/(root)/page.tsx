c1b254eba01195676ed13cd7c2eb922b9491f9d8
Wei-Lin Chou <wcia013twe@gmail.com>
2024-09-08 20:46:46 -0400

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import HeaderBox from '@/components/HeaderBox';
+
+const Home = () => {
+    return (
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox />   
+                </header>
+            </div>
+
+        </section>
+    )
+}
+
+export default Home;
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderBox = () => {
+  return (
+    <div>
+      HeaderBox
+    </div>
+  )
+}
+
+export default HeaderBox
