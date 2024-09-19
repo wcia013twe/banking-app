dcda150083aa69d4eb7060bde9e9138c36b9ecd2
wcia013twe <wcia013twe@gmail.com>
2024-09-19 00:34:59 -0400

    labtop first init

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,14 +1,17 @@
-import { formatAmount } from '@/lib/utils'
+// import { formatAmount } from '@/lib/utils'
 import React from 'react'
-import CountUp from 'react-countup'
+// import CountUp from 'react-countup'
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
 }: TotlaBalanceBoxProps) => {
   return (
     <section className="total-balance">
-        <div className="total-balance-chart"></div>
+        <div className="total-balance-chart">
+            <DoughnutChart accounts={accounts}/>
+        </div>
         <div className="flex flex-col gap-6">
             <h2 className="header-2">
                 Bank Accounts: {totalBanks}
