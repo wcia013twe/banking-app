16fe2220da411deb4fd67a0497067272d9ec87b0
wcia013twe <wcia013twe@gmail.com>
2024-10-13 19:18:00 -0400

    connected appwrite

--- lib/utils.ts
+++ lib/utils.ts
@@ -196,17 +196,16 @@ export const getTransactionStatus = (date: Date) => {
 };
 
 export const authFormSchema = (type: string) => z.object({
-  //sign-up
-  username:  type === 'sign-in'?  z.string().optional() : z.string().min(8),
+  // sign up
   firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  lastName: type === 'sign-in' ?z.string().optional() :  z.string().min(3),
-  address1:  type === 'sign-in'?  z.string().optional() : z.string().max(50),
-  state:  type === 'sign-in'?  z.string().optional() : z.string().min(2).min(2),
+  lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  city: type === 'sign-in' ? z.string().optional() : z.string().max(50),
+  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
   postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
   dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
-  ssn:  type === 'sign-in' ? z.string().optional() : z.string().min(3),
-
-  //both sign-up and sign-in
+  ssn: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+  // both
   email: z.string().email(),
   password: z.string().min(8),
-});
+})
