3bc3c3873818ee13ff809bb49616a2509c57c933
wcia013twe <wcia013twe@gmail.com>
2024-10-13 23:54:35 -0400

    solved react type error

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -49,20 +49,8 @@ const AuthForm = ({ type }: { type: string }) => {
         // Sign up with Appwrite & create plaid token
         
         if(type === 'sign-up') {
-          const userData = {
-            firstName: data.firstName!,
-            lastName: data.lastName!,
-            address1: data.address1!,
-            city: data.city!,
-            state: data.state!,
-            postalCode: data.postalCode!,
-            dateOfBirth: data.dateOfBirth!,
-            ssn: data.ssn!,
-            email: data.email,
-            password: data.password
-          }
 
-          const newUser = await signUp(userData);
+          const newUser = await signUp(data);
 
           setUser(newUser);
         }
